cd382352d01fc364924c76562169ecda58287537
Efrain Arenas <efrain_escuela@outlook.com>
2021-05-15 13:21:28 -0500

    Migración de funciones a la carpeta Negocio

--- src/negocio/Publicacion.py
+++ src/negocio/Publicacion.py
@@ -1,33 +1,36 @@
-from datetime import datetime
+from sqlalchemy import Column, Integer, String, Date, ForeignKey
+from sqlalchemy.orm import relationship, Session
 
-from sqlalchemy import Column, String, Integer, DateTime
-from sqlalchemy.exc import SQLAlchemyError
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import Session
+from src.negocio import EstadoPublicacion
 from src.datos.Conexion import Conexion
-
-Base = declarative_base()
 
 
 class Publicacion(Conexion):
-    __tablename__ = "Publicacion"
+	__tablename__ = "Publicacion"
 
-    idPublicacion: Column = Column(Integer(), primary_key=True, nullable=False)
-    titulo: Column = Column(String(200), nullable=False, unique=False)
-    descripcion: Column = Column(String(500), nullable=False, unique=False)
-    estado: Column = Column(Integer(), nullable=False, unique=False)
-    categoria: Column = Column(Integer(), nullable=False, unique=False)
-    publicador: Column = Column(Integer(), nullable=False, unique=False)
-    fechaCreacion: Column = Column(DateTime(), nullable=False, unique=False)
-    fechaFin: Column = Column(DateTime(), nullable=False, unique=False)
+	idPublicacion: Column = Column(Integer(), primary_key=True, nullable=False)
+	titulo: Column = Column(String(200), nullable=False)
+	descripcion: Column = Column(String(500), nullable=False)
+	estado: Column = Column(Integer(), nullable=False, default=1)
+	fechaCreacion: Column = Column(Date(), nullable=False)
+	fechaFin: Column = Column(Date(), nullable=False)
+	referencia = relationship("Miembro", back_populates="referenciaPublicaciones")
+	publicador: Column(Integer(), ForeignKey("MiembroOfercompas.idMiembro"), nullable=False)
 
-    def __init__(self):
-        self.idPublicacion = None
-        self.titulo = None
-        self.descripcion = None
-        self.estado = None
-        self.categoria = None
-        self.publicador = None
-        self.fechaCreacion = None
-        self.fechaFin = None
+	def __init__(self):
+		self.idPublicacion = 0
+		self.titulo = None
+		self.descripcion = None
+		self.estado = EstadoPublicacion.ACTIVO
+		self.fechaCreacion = None
+		self.fechaCreacion = None
+		self.publicador = None
+
+	def estaRegistrada(self) -> bool:
+		conexion: Session = Publicacion.abrir_conexion()
+		repetido: bool = conexion.query(
+			conexion.query(Publicacion).filter_by(titulo=self.titulo, descripcion=self.descripcion).exists()
+		).scalar()
+		conexion.close()
+		return repetido
 
