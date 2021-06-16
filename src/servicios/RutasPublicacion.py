1acc111b3f23e8103f2e43f78153ea5dba7d2012
rendon.luisegerardo@gmail.com <LuisGerardoRendon>
2021-06-16 03:32:03 -0500

    Implementación del reporte y refactorizacion de enpoints para comentarios y denuncias

--- src/servicios/RutasPublicacion.py
+++ src/servicios/RutasPublicacion.py
@@ -4,6 +4,8 @@ from http import HTTPStatus
 
 from flask import Blueprint, Response, request, send_file
 
+from src.negocio.Comentario import Comentario
+from src.negocio.Denuncia import Denuncia
 from src.negocio.Oferta import Oferta
 from src.negocio.Publicacion import Publicacion
 from src.negocio.Puntuacion import Puntuacion
@@ -19,21 +21,6 @@ def eliminar_publicacion(idPublicacion):
     return Response(status=status)
 
 
-@rutas_publicacion.route("/publicaciones/<idPublicacion>/interaccion", methods=["GET"])
-def obtener_interaccion(idPublicacion):
-    print("OBTENIENDO INTERACCION")
-    payload = request.json
-    print(request)
-    print(payload)
-    valores_requeridos = ["idMiembro"]
-    if payload is not None:
-        if all(llave in payload for llave in valores_requeridos):
-            id_miembro = payload.get("idMiembro")
-            respuesta = Response(json.dumps(Publicacion.obtener_interaccion(id_miembro, idPublicacion)),
-                             status=HTTPStatus.OK)
-    else:
-        respuesta = Response(status=HTTPStatus.NOT_FOUND)
-    return respuesta
 
 
 @rutas_publicacion.route("/publicaciones/<idPublicacion>/puntuaciones", methods=["POST"])
@@ -138,4 +125,62 @@ def obtener_interaccion(idPublicacion):
                              status=HTTPStatus.OK, mimetype="application/json")
     else:
         respuesta = Response(status=HTTPStatus.NOT_FOUND)
+    return respuesta
+
+@rutas_publicacion.route("/publicaciones/<id_publicacion>/comentarios", methods=["POST"])
+def registrar_comentario(id_publicacion):
+    comentario_recibido = request.json
+    valores_requeridos = {"idMiembro", "contenido"}
+    print(comentario_recibido)
+    respuesta = Response(status=HTTPStatus.BAD_REQUEST)
+    if comentario_recibido is not None:
+        if all(llave in comentario_recibido for llave in valores_requeridos):
+            comentario = Comentario()
+            comentario.instanciar_con_hashmap(comentario_recibido, id_publicacion)
+            resultado = comentario.registrar()
+            if resultado == HTTPStatus.CREATED:
+                respuesta = Response(comentario.hacer_json_sin_nickname(),
+                                     status=resultado,
+                                     mimetype="application/json")
+
+            else:
+                respuesta = Response(status=resultado)
+    return respuesta
+
+
+@rutas_publicacion.route("/publicaciones/<id_publicacion>/comentarios", methods=["GET"])
+def obtener_comentarios(id_publicacion):
+    respuesta = Response(status=HTTPStatus.BAD_REQUEST)
+    if id_publicacion is not None:
+        comentarios = Comentario.obtener_comentarios(id_publicacion)
+        if comentarios:
+            array_comentarios = []
+            for comentario in comentarios:
+                array_comentarios.append(comentario.hacer_json_nickname_contenido())
+            comentarios_json = json.dumps(array_comentarios)
+            print(comentarios_json)
+            respuesta = Response(comentarios_json, status=HTTPStatus.OK, mimetype="application/json")
+        else:
+            respuesta = Response(status=HTTPStatus.NOT_FOUND)
+    return respuesta
+
+
+@rutas_publicacion.route("/publicaciones/<id_publicacion>/denuncias", methods=["POST"])
+def registrar_denuncia(id_publicacion):
+    denuncia_recibida = request.json
+    valores_requeridos = {"idDenunciante", "comentario", "motivo"}
+    print(denuncia_recibida)
+    respuesta = Response(status=HTTPStatus.BAD_REQUEST)
+    if denuncia_recibida is not None:
+        if all(llave in denuncia_recibida for llave in valores_requeridos):
+            denuncia = Denuncia()
+        denuncia.instanciar_con_hashmap(denuncia_recibida, id_publicacion)
+        resultado = denuncia.registrar()
+        if resultado == HTTPStatus.CREATED:
+            respuesta = Response(denuncia.hacer_json(),
+                                 status=resultado,
+                                 mimetype="application/json")
+
+        else:
+            respuesta = Response(status=resultado)
     return respuesta
